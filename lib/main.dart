9c6c8e7db63fe80b075d9d73fcc93c6f58ec6da6
Admin <Admin@DESKTOP-9MILIL3>
2022-03-31 10:31:10 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:baitapbuoi5/detail_product.dart';
+import 'package:baitapbuoi5/product_list.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class Routes {
+  static final String product = "/product";
+  static final String detail = "/detail";
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+      ),
+      initialRoute: Routes.product,
+      routes: {
+        Routes.product: (context) => ProductList(),
+      },
+    );
+  }
+}
+
+
